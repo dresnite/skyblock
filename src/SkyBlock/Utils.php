1a0c8ae0937378b1f9f5e2d810d33f735d8e89a1
95CivicSi <aaron_salyer@yahoo.com>
2018-07-17 21:41:06 -0400

    Util: Add utility to return an actual instance of a SkyBlockIslandGenerator.   This makes it easier to integrate multiple generator types.

--- src/SkyBlock/Utils.php
+++ src/SkyBlock/Utils.php
@@ -2,10 +2,12 @@
 
 namespace SkyBlock;
 
+use pocketmine\level\generator\GeneratorManager;
 use pocketmine\level\Level;
 use pocketmine\level\Position;
 use pocketmine\Server;
 use pocketmine\utils\Config;
+use SkyBlock\generator\SkyBlockGenerator;
 
 class Utils {
 
@@ -83,6 +85,23 @@ class Utils {
         else {
             return null;
         }
+    }
+
+    /**
+     * Compares a string to available world names.  If the matching world is a SkyBlock island,
+     * this will return a copy of the matching SkyBlockIslandGenerator, otherwise it returns null.
+     *
+     * @param string $islandName
+     * @return null|SkyBlockGenerator
+     */
+    public static function getIslandGenerator(string $islandName): ?SkyBlockGenerator {
+        $level = Server::getInstance()->getLevelByName($islandName);
+        $islandGeneratorName = GeneratorManager::getGenerator($level->getProvider()->getGenerator());
+        $islandGenerator = new $islandGeneratorName();
+        if($islandGenerator instanceof SkyBlockGenerator){
+            return $islandGenerator;
+        }
+        return null;
     }
 
 }
