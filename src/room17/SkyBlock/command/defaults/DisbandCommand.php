9be89e6723be6ad4a304e488c5c86a7897a77e14
andreskomet <giantquartz@gmail.com>
2018-11-21 14:55:46 +0100

    Update some headers

--- src/room17/SkyBlock/command/defaults/DisbandCommand.php
+++ src/room17/SkyBlock/command/defaults/DisbandCommand.php
@@ -1,8 +1,17 @@
 <?php
 /**
- * Copyright (C) Andrés Arias - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ *  _____    ____    ____   __  __  __  ______
+ * |  __ \  / __ \  / __ \ |  \/  |/_ ||____  |
+ * | |__) || |  | || |  | || \  / | | |    / /
+ * |  _  / | |  | || |  | || |\/| | | |   / /
+ * | | \ \ | |__| || |__| || |  | | | |  / /
+ * |_|  \_\ \____/  \____/ |_|  |_| |_| /_/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
  */
 
 namespace room17\SkyBlock\command\defaults;
