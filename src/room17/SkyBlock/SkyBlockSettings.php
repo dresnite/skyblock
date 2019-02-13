ba73462191b6e6b700375ed3e1e8f3d816d4b8e5
andres <giantquartz@gmail.com>
2019-02-13 21:04:02 +0100

    SkyBlockSettings: Fix typo

--- src/room17/SkyBlock/SkyBlockSettings.php
+++ src/room17/SkyBlock/SkyBlockSettings.php
@@ -133,7 +133,7 @@ class SkyBlockSettings {
         }
         $this->cooldownDuration = $this->data["cooldown-duration-minutes"] ?? 20;
         $this->preventVoidDamage = $this->data["prevent-void-damage"] ?? true;
-        $this->isleBlockedCommands = $this->data["commands-blocked-in-islands"] ?? [];
+        $this->isleBlockedCommands = $this->data["commands-blocked-in-isles"] ?? [];
         $this->isleBlockedCommands = array_map("strtolower", $this->isleBlockedCommands);
     }
     
