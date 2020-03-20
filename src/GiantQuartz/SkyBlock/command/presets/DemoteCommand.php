bf27b45e4a96f9e7db3e73f69f9f15a793465096
GiantQuartz <giantquartz@gmail.com>
2020-03-20 16:20:23 +0100

    DemoteCommand: Clean up and fix logical error

--- src/GiantQuartz/SkyBlock/command/presets/DemoteCommand.php
+++ src/GiantQuartz/SkyBlock/command/presets/DemoteCommand.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 namespace GiantQuartz\SkyBlock\command\presets;
 
 
+use GiantQuartz\SkyBlock\session\OfflineSession;
 use GiantQuartz\SkyBlock\session\SessionLocator;
 use ReflectionException;
 use GiantQuartz\SkyBlock\command\IslandCommand;
@@ -35,59 +36,85 @@ class DemoteCommand extends IslandCommand {
      * @throws ReflectionException
      */
     public function onCommand(Session $session, array $args): void {
-        if($this->checkLeader($session)) {
-            return;
-        } elseif(!isset($args[0])) {
-            $session->sendTranslatedMessage(new MessageContainer("DEMOTE_USAGE"));
+        if($this->checkLeader($session) or $this->checkArgs($session, $args)) {
             return;
         }
 
         $offlineSession = SessionLocator::getOfflineSession($args[0]);
-        if($this->checkClone($session, $offlineSession->getSession())) {
+        if($this->checkClone($session, $offlineSession->getSession()) or $this->checkDifferentIsland($session, $offlineSession)) {
             return;
-        } elseif($offlineSession->getIslandId() != $session->getIslandId()) {
-            $session->sendTranslatedMessage(new MessageContainer("MUST_BE_PART_OF_YOUR_ISLAND", [
-                "name" => $args[0]
-            ]));
-        } else {
-            $rank = null;
-            $rankName = "";
-            switch($offlineSession->getRank()) {
-                case Session::RANK_OFFICER:
-                    $rank = Session::RANK_DEFAULT;
-                    $rankName = "MEMBER";
-                    break;
-                case Session::RANK_LEADER:
-                    $rank = Session::RANK_OFFICER;
-                    $rankName = "OFFICER";
-                    break;
-                case Session::RANK_FOUNDER:
-                    $rank = false;
-            }
-            if($rank == null) {
-                $session->sendTranslatedMessage(new MessageContainer("CANNOT_DEMOTE_MEMBER", [
-                    "name" => $args[0]
-                ]));
-                return;
-            } elseif($rank == false) {
-                $session->sendTranslatedMessage(new MessageContainer("CANNOT_DEMOTE_FOUNDER"));
-                return;
-            }
-            $onlineSession = $offlineSession->getSession();
-            if($onlineSession != null) {
-                $onlineSession->setRank($rank);
-                $onlineSession->sendTranslatedMessage(new MessageContainer("YOU_HAVE_BEEN_DEMOTED"));
-                $onlineSession->save();
-            } else {
-                $offlineSession->setRank($rank);
-                $offlineSession->save();
-            }
-            $session->sendTranslatedMessage(new MessageContainer("SUCCESSFULLY_DEMOTED_PLAYER", [
-                "name" => $args[0],
-                "to" => $session->getMessage(new MessageContainer("$rankName"))
-            ]));
         }
 
+        $lowerRank = $this->getLowerRank($offlineSession->getRank());
+
+        if($lowerRank !== null) {
+            $offlineSession->setRank($lowerRank);
+            $offlineSession->save();
+            $this->sendDemoteMessage($offlineSession);
+
+            $session->sendTranslatedMessage(new MessageContainer("SUCCESSFULLY_DEMOTED_PLAYER", [
+                "name" => $args[0],
+                "to" => $session->getMessage(new MessageContainer($this->getRankName($lowerRank)))
+            ]));
+        } elseif($offlineSession->getRank() == Session::RANK_FOUNDER) {
+            $session->sendTranslatedMessage(new MessageContainer("CANNOT_DEMOTE_FOUNDER"));
+        } else {
+            $session->sendTranslatedMessage(new MessageContainer("CANNOT_DEMOTE_MEMBER", [
+                "name" => $args[0]
+            ]));
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function sendDemoteMessage(OfflineSession $session): void {
+        $online = $session->getSession();
+        if($online != null) {
+            $online->sendTranslatedMessage(new MessageContainer("YOU_HAVE_BEEN_DEMOTED"));
+        }
+    }
+
+    private function getLowerRank(int $rank): ?int {
+        switch($rank) {
+            case Session::RANK_OFFICER:
+                return Session::RANK_DEFAULT;
+            case Session::RANK_LEADER:
+                return Session::RANK_OFFICER;
+        }
+        return null;
+    }
+
+    private function getRankName(int $rank): ?string {
+        switch($rank) {
+            case Session::RANK_DEFAULT:
+                return "MEMBER";
+            case Session::RANK_OFFICER:
+                return "OFFICER";
+            case Session::RANK_LEADER:
+                return "LEADER";
+            case Session::RANK_FOUNDER:
+                return "FOUNDER";
+        }
+        return null;
+    }
+
+    private function checkArgs(Session $session, array $args): bool {
+        if(count($args) < 1) {
+            $session->sendTranslatedMessage(new MessageContainer("DEMOTE_USAGE"));
+            return true;
+        }
+        return false;
+    }
+
+    private function checkDifferentIsland(Session $session, OfflineSession $offlineSession): bool {
+        if($offlineSession->getIslandId() != $session->getIslandId()) {
+            $session->sendTranslatedMessage(new MessageContainer("MUST_BE_PART_OF_YOUR_ISLAND", [
+                "name" => $offlineSession->getLowerCaseName()
+            ]));
+            return true;
+        }
+        return false;
     }
 
 }
