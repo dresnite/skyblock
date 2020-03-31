7b54062ef78328c5eecd205900cd5979c0729d3b
GiantQuartz <giantquartz@gmail.com>
2020-03-31 22:37:03 +0200

    Add missing header to RankIds

--- src/room17/SkyBlock/island/RankIds.php
+++ src/room17/SkyBlock/island/RankIds.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
 
 namespace room17\SkyBlock\island;
 
