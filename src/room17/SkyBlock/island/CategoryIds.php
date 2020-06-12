c76ad36ae06250df912b51d4c25721b1e4ba46bb
GiantQuartz <giantquartz@gmail.com>
2020-06-12 08:33:18 +0200

    Make CategoryIds and RankIds final classes with private ctors

--- src/room17/SkyBlock/island/CategoryIds.php
+++ src/room17/SkyBlock/island/CategoryIds.php
@@ -9,12 +9,16 @@
 namespace room17\SkyBlock\island;
 
 
-interface CategoryIds {
+final class CategoryIds {
 
     public const EXTRA_SMALL = "XS";
     public const SMALL = "S";
     public const MEDIUM = "M";
     public const LARGE = "L";
     public const EXTRA_LARGE = "XL";
+
+    private function __construct() {
+        // NOOP
+    }
 
 }
--- src/room17/SkyBlock/island/RankIds.php
+++ src/room17/SkyBlock/island/RankIds.php
@@ -9,11 +9,15 @@
 namespace room17\SkyBlock\island;
 
 
-interface RankIds {
+final class RankIds {
 
     public const MEMBER = 0;
     public const OFFICER = 1;
     public const LEADER = 2;
     public const FOUNDER = 3;
+
+    private function __construct() {
+        // NOOP
+    }
 
 }
